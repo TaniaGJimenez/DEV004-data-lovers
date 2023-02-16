ef90bbb8af687a1351bd9f07d4ad9b4cc5373e37
Esperaanza <xcaanda100@gmail.com>
2023-02-16 15:34:52 -0600

    Elaboracion de los primeros test

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 // estas funciones son de ejemplo
 
-export const Imagenes = () => {
-  return "Imagenes";
-};
+// export const Imagenes = () => {
+//   return "Imagenes";
+// };
 
-export const anotherExample = () => {
-  return "OMG";
-};
+// export const anotherExample = () => {
+//   return "OMG";
+// };
 export const tarjetas = (data) => {
   let tarjetaString = "";
   for (const pokemon of data.pokemon) {
     tarjetaString += `
       <section class="tarjeta">
         <img class="imagenPokemon" src=${pokemon.img} >
-        <p>#${pokemon.num}<br>${pokemon.name.toUpperCase()}<\p> 
+        <p>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</p> 
       </section>
     `;
   }
@@ -23,11 +23,15 @@ export const filtrarTipos = (data, tipo) => {
   const dataFiltrada = data.pokemon.filter((pokemon) =>
     pokemon.type.includes(tipo)
   );
-  console.log(dataFiltrada);
+  //console.log(dataFiltrada);
   return { pokemon: dataFiltrada };
 };
 
 export const buscarPorNombre = (data, nombreBuscar) => {
-  const dataFiltrada = data.pokemon.filter((pokemon) => pokemon.name.startsWith(nombreBuscar))
+  const dataFiltrada = data.pokemon.filter((pokemon) => pokemon.name===(nombreBuscar))
   return { pokemon: dataFiltrada }
 };
+// export const buscarPorNombre1 = (data, tipo) => {
+//   const dataFiltrada = data.pokemon.search((pokemon) =>  pokemon.name.includes(nombreBuscar))
+//   return { pokemon: dataFiltrada }
+// };
--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import { Imagenes,tarjetas,filtrarTipos,buscarPorNombre} from './data.js';
+import {tarjetas,filtrarTipos,buscarPorNombre} from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-const pokemonDB = data.pokemon
+//const pokemonDB = data.pokemon
 
 // demostracion de datos de pokemon.js a main.js
-console.log("pokemonDB =")
-console.log(pokemonDB)
-console.log(Imagenes)
-console.log(data);
+// console.log("pokemonDB =")
+// console.log(pokemonDB)
+// console.log(Imagenes)
+// console.log(data);
 //const btn = document.getElementById("btn");
 //const dataContainer = document.getElementById("dataContainer");
 const bloqueTarjetas = document.getElementById("bloqueTarjetas");
 bloqueTarjetas.innerHTML = tarjetas(data)
-const tipos=document.querySelector(".tipos");
+//const tipos=document.querySelector(".tipos");
 const tipoPokemon=document.getElementsByClassName("tipoPokemon")
-const menuTipos=document.querySelector(".menu-tipos")
+//const menuTipos=document.querySelector(".menu-tipos")
 const inputBuscar = document.getElementById("inputBuscar")
 /*btn.addEventListener("click", function() {
   // Mostrar o ocultar el contenedor de datos
@@ -29,8 +29,8 @@ const inputBuscar = document.getElementById("inputBuscar")
 for (const tipo of tipoPokemon) {
   tipo.addEventListener("click", () => {
     //el console seria sustituido por el llamado a funcion que realice el filtrado his1
-    console.log(data.pokemon)
-    console.log(tipo.name.toString())
+    // console.log(data.pokemon)
+    // console.log(tipo.name.toString())
     // menuTipos.style.display = 'none'
     const pokemonesTipo = filtrarTipos(data, tipo.name)
     bloqueTarjetas.innerHTML = tarjetas(pokemonesTipo)
@@ -38,8 +38,12 @@ for (const tipo of tipoPokemon) {
 }
 
 inputBuscar.addEventListener('input', () => {
-  console.log(inputBuscar.value)
+  //console.log(inputBuscar.value)
   const FiltroNombre = buscarPorNombre(data, inputBuscar.value)
   bloqueTarjetas.innerHTML = tarjetas(FiltroNombre)
 });
-
+// inputBuscar.addEventListener('input', () => {
+//   console.log(inputBuscar.value)
+//   const FiltroNombre = buscarPorNombre1(data, inputBuscar.value)
+//   bloqueTarjetas.innerHTML = tarjetas(FiltroNombre)
+// });
