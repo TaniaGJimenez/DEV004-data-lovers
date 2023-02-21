a83468b255cff7fb007613331f35c3ddc5a64231
Esperaanza <xcaanda100@gmail.com>
2023-02-21 15:20:13 -0600

    los datos para la grafica

--- src/data.js
+++ src/data.js
@@ -1,12 +1,4 @@
-// estas funciones son de ejemplo
 
-// export const Imagenes = () => {
-//   return "Imagenes";
-// };
-
-// export const anotherExample = () => {
-//   return "OMG";
-// };
 export const tarjetas = (data) => {
   let tarjetaString = "";
   for (const pokemon of data.pokemon) {
@@ -14,8 +6,7 @@ export const tarjetas = (data) => {
       <section class="tarjeta">
         <img class="imagenPokemon" src=${pokemon.img} >
         <p>#${pokemon.num}<br>${pokemon.name.toUpperCase()}</p> 
-        <div class="pokemon-tipos">
-        ${pokemon.type}
+        
     </div>
     </section>
     `;
@@ -26,7 +17,7 @@ export const filtrarTipos = (data, tipo) => {
   const dataFiltrada = data.pokemon.filter((pokemon) =>
     pokemon.type.includes(tipo)
   );
-  //console.log(dataFiltrada);
+
   return { pokemon: dataFiltrada };
 };
 
@@ -45,7 +36,6 @@ export function abcOrder(data, order) {
     else {
       return 1;
     }
-    //return 0;
   })
   if (order === 'AZ') {
     return {pokemon:orderArray}  
@@ -54,18 +44,3 @@ export function abcOrder(data, order) {
     return {pokemon:inverso}
   }
 }
-export const ordenarArregloNumero = (clave, orden, data) => {
-  const dataOrdenada = {}
-  if (orden === 'ascendente') {
-    dataOrdenada.pokemon = data.pokemon.sort((a, b) => Number(a[clave]) - Number(b[clave]))
-  } else {
-    dataOrdenada.pokemon = data.pokemon.sort((a, b) => Number(b[clave]) - Number(a[clave]))
-  }
-  return dataOrdenada;
-};
-
-export const cortarTop = (dataOrdenada, top) => {
-  const Top10 = {}
-  Top10.pokemon = dataOrdenada.pokemon.slice(0, top)
-  return Top10;
-};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-import {tarjetas,filtrarTipos,buscarPorNombre,abcOrder} from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
-import data from './data/pokemon/pokemon.js';
+import { tarjetas, filtrarTipos, buscarPorNombre, abcOrder } from "./data.js";
+
+import data from "./data/pokemon/pokemon.js";
 
 const todos = {
-  pokemon: [
-    ...data.pokemon
-  ]
+  pokemon: [...data.pokemon],
+};
 
-}
-
+//import pokemon from './data/pokemon/pokemon.js';
 //const pokemonDB = data.pokemon
 //OrdenarPorNombre,OrdenarPorNombreAZ,
 // demostracion de datos de pokemon.js a main.js
@@ -22,22 +20,27 @@ const todos = {
 //const menuTipos=document.querySelector(".menu-tipos")
 
 const bloqueTarjetas = document.getElementById("bloqueTarjetas");
-bloqueTarjetas.innerHTML = tarjetas(data)
-const tipoPokemon=document.getElementsByClassName("tipoPokemon")
-const inputBuscar = document.getElementById("inputBuscar")
-const Ordenar = document.getElementsByClassName("Ordenar")
+bloqueTarjetas.innerHTML = tarjetas(data);
+const tipoPokemon = document.getElementsByClassName("tipoPokemon");
+const inputBuscar = document.getElementById("inputBuscar");
+const Ordenar = document.getElementsByClassName("Ordenar");
+const grafica = document.getElementById("grafica");
 
 for (const tipo of tipoPokemon) {
   tipo.addEventListener("click", () => {
-    const pokemonesTipo = filtrarTipos(data, tipo.name)
-    bloqueTarjetas.innerHTML = tarjetas(pokemonesTipo)
-  })
+    if (tipo.name === "todos") {
+      bloqueTarjetas.innerHTML = tarjetas(todos);
+    } else {
+      const pokemonesTipo = filtrarTipos(data, tipo.name);
+      bloqueTarjetas.innerHTML = tarjetas(pokemonesTipo);
+    }
+  });
 }
 
-inputBuscar.addEventListener('input', () => { 
+inputBuscar.addEventListener("input", () => {
   //console.log(inputBuscar.value)
-  const FiltroNombre = buscarPorNombre(data, inputBuscar.value)
-  bloqueTarjetas.innerHTML = tarjetas(FiltroNombre)
+  const FiltroNombre = buscarPorNombre(data, inputBuscar.value);
+  bloqueTarjetas.innerHTML = tarjetas(FiltroNombre);
 });
 // inputBuscar.addEventListener('input', () => {
 //   console.log(inputBuscar.value)
@@ -50,25 +53,44 @@ for (const item of Ordenar) {
     if (item.name === "todos") {
       bloqueTarjetas.innerHTML = tarjetas(todos);
     }
-    
+
     if (item.name === "AZ") {
-      const SortData = abcOrder (data, item.name)
-      bloqueTarjetas.innerHTML = tarjetas(SortData)
+      const SortData = abcOrder(data, item.name);
+      bloqueTarjetas.innerHTML = tarjetas(SortData);
+    } else if (item.name === "ZA") {
+      const SortData = abcOrder(data, item.name);
+      bloqueTarjetas.innerHTML = tarjetas(SortData);
     }
-    else if (item.name === "ZA") {
-      const SortData = abcOrder (data, item.name)
-      bloqueTarjetas.innerHTML = tarjetas(SortData)
-    }
-  })
+  });
 }
+grafica.addEventListener("click", () => {
+  const arrayTypes = [
+    "psychic",
+    "ground",
+    "water",
+    "fighting",
+    "normal",
+    "ghost",
+    "grass",
+    "poison",
+    "flying",
+    "dark",
+    "fairy",
+    "dragon",
+    "rock",
+    "steel",
+    "ice",
+    "electric",
+  ];
+  const arrayTotalTypes = [];
 
-const tipo= data.pokemon.reduce((acumulador, tipos) => {
-  if (tipos.type === 'psychic') {
-    return acumulador + 1
-    
-  } else {
-    return acumulador
+  for (let index = 0; index < arrayTypes.length; index++) {
+    console.log(filtrarTipos(data, arrayTypes[index]).pokemon.length);
+    //objTotalTypes.arrayTypes[index] = filtrarTipos(data,arrayTypes[index]).pokemon.length
+    arrayTotalTypes.push({
+      type: arrayTypes[index],
+      total: filtrarTipos(data, arrayTypes[index]).pokemon.length,
+    });
   }
-}, 1)
-
-console.log(tipo)
+  console.log(arrayTotalTypes);
+});
